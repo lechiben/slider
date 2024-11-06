0e139e778bc287b30fdffa0d51202e2b54a56411
Ben Le <lechiben@hotmail.com>
2024-11-05 22:07:56 -0800

    finish styling the slider

---
+++ main.js
@@ -0,0 +1,27 @@
+const left = document.querySelector(".left") 
+const right = document.querySelector(".right") 
+const slider = document.querySelector(".slider") 
+const images = document.querySelectorAll(".image")
+
+
+let slideNumber = 1;
+const length = images.length;
+
+const nextSlide = () => {
+    slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+    slideNumber++;   
+};
+
+const getFirstSlide = () => {
+    slider.style.transform = `translateX(0px)`;
+    slideNumber = 1;
+};
+
+right.addEventListener("click", () => {
+    slideNumber < length ? nextSlide() : getFirstSlide();
+});
+
+left.addEventListener("click" = () => 
+{ 
+
+})
